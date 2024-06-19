75ea6f05869c03fc2a19827ecee52e213249dc72
MykolaDekusar <ndekusar97@gmail.com>
2024-06-19 17:22:46 +0200

    [UPD]Created starting mockup

--- index.php
+++ index.php
@@ -1,15 +1,40 @@
-<?php 
-$nome = $_GET['nome'];
-echo ($nom) ;
+<?php
+$l_password = $_GET['lunghezzaP'] ?? 0;
+
+echo ($l_password);
+function passwordGenerator($length)
+{
+    $password = '';
+    for ($i = 0; $i < $length; $i++) {
+        $password .= 2;
+    }
+    return $password;
+}
+$password = passwordGenerator($l_password);
+echo $password;
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Strong PAssword Generator</title>
 </head>
+
 <body>
-    
+    <h1>Strong Password Generator</h1>
+    <section>
+        <form action="index.php" method="GET">
+            <label for="lunghezzaP">Inserisci lunghezza password</label>
+            <input type="text" name="lunghezzaP">
+            <button>Invia</button>
+        </form>
+    </section>
+    <section>
+        <h2>Ecco la tua password:</h2>
+        <p>Hai inserito <?php echo $l_password ?> caratteri</p>
+    </section>
 </body>
+
 </html>
